dfda9c370f21d161bca51297d6b28efed460b969
shannonpearson <shannon.pearson@doctolib.com>
2021-01-16 20:54:47 +0100

    add documentation

--- lib/usego.rb
+++ lib/usego.rb
@@ -1,6 +1,16 @@
 require_relative './usego/golang_helper'
 
 class Usego
+  # Add two numbers together, using a Golang function
+  #
+  # Example:
+  #  >> Usego.goAdd(2, 3)
+  #  => 5
+  #
+  # Arguments:
+  #   a: (Integer)
+  #   b: (Integer)
+
   def self.goAdd(a = 0, b = 0)
     GolangHelper.GoAdd(a, b)
   end
